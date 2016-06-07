be0e464dc37243b2f9ee2c1a08b74a355b3b5cc6
shim_ <marvin@localhost.localdomain>
2016-06-07 15:36:27 +0200

    added option for seperate key and cert files

--- start.sh
+++ start.sh
@@ -2,12 +2,18 @@
 
 set -e
 
-# check if we have a PEM file, otherwise generate a self-signed one
-if [ ! -f $HITCH_PEM ]
-then
+# check if we have a PEM file or it's two components, otherwise generate a self-signed one
+if [[ -f $HITCH_KEY && -f $HITCH_CERT ]] && [[ ! ( -z $HITCH_PEM && -f $HITCH_PEM) ]]; then
+  HITCH_PEM=/etc/ssl/hitch/combined.pem
+  touch $HITCH_PEM
+  chmod 440 $HITCH_PEM
+  cat $HITCH_KEY $HITCH_CERT > $HITCH_PEM
+  echo Combined $HITCH_KEY and $HITCH_CERT
+elif [ -f $HITCH_PEM ]; then
+  echo Using $HITCH_PEM
+else
   # check if $DOMAIN is set, otherwise set to example.com
-  if [ -z $DOMAIN ]
-  then
+  if [ -z $DOMAIN ]; then
       export DOMAIN="example.com"
   fi  
 
@@ -15,7 +21,6 @@ then
   cd /etc/ssl/hitch
   openssl req -newkey rsa:2048 -sha256 -keyout example.com.key -nodes -x509 -days 365 -out example.crt -subj "/C=CH/ST=Zurich/L=Zurich/O=Snakeoil Inc/OU=IT Department/CN=$DOMAIN"
   cat example.com.key example.crt > combined.pem
-
 fi
 
 
